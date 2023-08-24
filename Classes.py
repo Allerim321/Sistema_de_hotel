a10094888a48da39b03044063c8819c429655ff2
Mirella <mirellatcg@gmail.com>
2023-08-24 08:15:54 -0300

    up

--- Classes.py
+++ Classes.py
@@ -1,31 +1,31 @@
 class Hotel:
-    def hotel():
-        print("")
+    def hotel(self):
+        print ("")
     
 class Quartos(Hotel):
-    def add_quarto():
+    def add_quarto(self, ocupado=0):
         print("")
 
 class Ap_luxo(Quartos):
-    def listar_ap_luxo():
+    def listar_ap_luxo(self, ocupado=0):
         print("")
 
 class Ap_Master(Quartos):
-    def listar_ap_master():
+    def listar_ap_master(self, ocupado=0):
         print("")
 
 class Ap_Simples(Quartos):
-    def listar_ap_simples():
+    def listar_ap_simples(self, ocupado=0):
         print("")
 
 class Ap_Simples_casal(Quartos):
-    def listar_ap_simples_casal():
+    def listar_ap_simples_casal(self, ocupado=0):
         print("")
 
 class Ap_Duplo(Quartos):
-    def listar_ap_duplo():
+    def listar_ap_duplo(self, ocupado=0):
         print("")
 
 class Ap_Duplo_casal(Quartos):
-    def listar_ap_duplo_casal():
+    def listar_ap_duplo_casal(self, ocupado=0):
         print("")
