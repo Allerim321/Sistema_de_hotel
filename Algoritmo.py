810ba512205cb1f6764ed281ec9f1bc076ca825f
pablo11-dev <pablofr2018@gmail.com>
2023-08-24 08:09:18 -0300

    comit menu1

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,4 +1,27 @@
 from Classes import *
 
 def main():
-    print("")
+    m = 1
+    while m == 1:
+        # Tratamento de erro
+           
+            print("Bem-vindos a um refúgio de elegância e conforto ")
+            print("[01] - ")
+            print("[02] - ")
+            print("[03] - ")
+            print("[04] - ")
+            print("[05] - ")
+            print("[06] - ")
+            print("[07] -")            
+            print("[08] - ")
+            print("[09] - ")
+            print("[10] - ")
+            print("[11] - ")
+            print("[12] - ")
+
+            menu=input("Digite o número correspondente ao quarto que gostaria de visitar: ")
+
+            match menu:
+                case "1, 01":
+                     ("Aqui estão algumas informações do quarto que você escolheu:")
+                
