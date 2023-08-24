3f6e01d1d4edd4243c3d0e33b9f41cc1211ed3f5
pablo11-dev <pablofr2018@gmail.com>
2023-08-24 09:43:21 -0300

    menu att

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,25 +1,25 @@
 from Classes import *
 import os
 
+def ver_quartos():
+    while True:
+        try:
+            print("Os quartos")
+
+        except:
+            print("Erro, opção inválida. Tente novamente.")
+            os.system("pause")
+            os.system("cls")
+
 def main():
     while True:
         try:
         # Tratamento de erro
            
             print("Bem-vindos a um refúgio de elegância e conforto ")
-            print("[01] - AP Simples")
-            print("[02] - AP Simples")
-            print("[03] - AP Simples Casal")
-            print("[04] - AP Simples Casal")
-            print("[05] - AP Duplo")
-            print("[06] - AP Duplo")
-            print("[07] - AP Duplo Casal")            
-            print("[08] - AP Duplo Casal")
-            print("[09] - AP Luxo")
-            print("[10] - AP Luxo")
-            print("[11] - AP Master")
-            print("[12] - AP Master")
-            print("[13] - Sair")
+            print("[01] - Ver quartos")
+            print("[02] - Realizar Hospedagem")
+            print("[03] - Sair")
 
             menu=input("Digite o número correspondente ao quarto que gostaria de visitar: ")
           
@@ -28,28 +28,11 @@ def main():
                     print("Você escolheu o AP simples algumas informações vão estar listadas abaixo: \n")
                 case "2":
                     print("Você escolheu o AP Simples algumas informações vão estar listadas abaixo: \n")
-                case "4, 04":
-                    print("Você escolheu o AP Simples Casal algumas informações vão estar listadas abaixo: \n")
-                case "5":
-                    print("Você escolheu o AP Duplo algumas informações vão estar listadas abaixo: \n")
-                case "6":
-                    print("Você escolheu o AP Duplo algumas informações vão estar listadas abaixo: \n")
-                case "7":
-                    print("Você escolheu o AP Duplo Casal algumas informações vão estar listadas abaixo: \n")
-                case "8":
-                    print("Você escolheu o AP Duplo Casal algumas informações vão estar listadas abaixo: \n")
-                case "9":
-                    print("Você escolheu o AP Luxo algumas informações vão estar listadas abaixo: \n")
-                case "10":
-                    print("Você escolheu o AP Luxo algumas informações vão estar listadas abaixo: \n")
-                case "11":
-                    print("Você escolheu o AP Master algumas informações vão estar listadas abaixo: \n")
-                case "12":
-                    print("Você escolheu o AP Master algumas informações vão estar listadas abaixo: \n")
+                case "3":
+                    print("Saindo...")
+                    os.system("pause")
+                    break
                     
-                case "13":
-                        exit()
-       
                 case _:
                     print ("Opção inválida")
                     os.system("pause")
@@ -60,6 +43,8 @@ def main():
         
         except:
             print("Erro, opção inválida. Tente novamente.")
+            os.system("pause")
+            os.system("cls") 
         
 
             
