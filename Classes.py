e9edffa935b3cac645989c160e96baba5aab82db
Mirella <mirellatcg@gmail.com>
2023-08-24 14:01:53 -0300

    as

--- Classes.py
+++ Classes.py
@@ -6,9 +6,11 @@ class Hotel:
         print ("")
     
 class Quartos(Hotel):
-    def reserva(self, nome, telefone, ocupado = 0):
+    def reserva(self, nome, telefone, data_inicio, data_fim, ocupado = 0):
         self.nome=nome
         self.telefone=telefone
+        self.data_inicio = data_inicio
+        self.data_fim = data_fim
 
         if ocupado == 1:
             print("Um quarto está reservado")
@@ -17,32 +19,25 @@ class Quartos(Hotel):
         
 
 class Ap_luxo(Quartos):
-    def listar_ap_luxo(self):
-        print("Este requintado apartamento de luxo é um verdadeiro exemplo de elegância e sofisticação. Localizado em um dos bairros mais exclusivos da cidade, oferece vistas deslumbrantes da paisagem urbana e do horizonte. Com um design arquitetônico contemporâneo e materiais de alta qualidade, cada detalhe deste apartamento foi cuidadosamente pensado para criar um ambiente de conforto e opulência.")
-        os.system("pause")
+    def informações(self):
+           print("Este requintado apartamento de luxo é um verdadeiro exemplo de elegância e sofisticação. Localizado em um dos bairros mais exclusivos da cidade, oferece vistas deslumbrantes da paisagem urbana e do horizonte. Com um design inovador contemporâneo e materiais de alta qualidade, cada detalhe deste apartamento foi cuidadosamente pensado para criar um ambiente de conforto e opulência.")
 
 class Ap_Master(Quartos):
-    def listar_ap_master(self):
-        print("Este magnífico apartamento master redefine o conceito de luxo e exclusividade. Localizado no topo de um edifício icônico, oferece uma vista panorâmica deslumbrante da cidade que se estende até onde os olhos podem alcançar. Projetado para aqueles que buscam o auge do conforto e sofisticação, este apartamento encapsula o estilo de vida elevado.")
-        os.system("pause")
-
+    def informações(self):
+        print("Este magnífico apartamento master redefine o conceito de luxo e exclusividade. Localizado no topo de um edifício icônico, oferece uma vista panorâmica deslumbrante da cidade que se estende até onde os olhos podem alcançar. Projetado para quem busca o auge do conforto e sofisticação, este apartamento encapsula o estilo de vida elevado.")
 
 class Ap_Simples(Quartos):
     def listar_ap_simples(self):
-        print("Este encantador apartamento simples é um espaço acolhedor e funcional, perfeito para aqueles que valorizam a simplicidade e a praticidade. Localizado em um bairro tranquilo, oferece uma atmosfera serena e aconchegante, ideal para relaxar após um longo dia.")
-        os.system("pause")
+       print("Este encantador apartamento simples é um espaço acolhedor e funcional, perfeito para quem valoriza a simplicidade e a praticidade. Localizado em um bairro tranquilo, oferece uma atmosfera serena e aconchegante, ideal para relaxar após um longo dia.")
 
 class Ap_Simples_casal(Quartos):
     def listar_ap_simples_casal(self):
-        print("Este charmoso apartamento simples de casal é um refúgio acolhedor e íntimo, projetado para acomodar o amor e a convivência. Localizado em um bairro tranquilo, oferece um espaço onde o conforto e a funcionalidade se unem de maneira harmoniosa.")
-        os.system("pause")
+        print("Este charmoso apartamento simples de casal é um refúgio acolhedor e íntimo, projetado para acomodação o amor e a convivência. Localizado em um bairro tranquilo, oferece um espaço onde o conforto e a funcionalidade são de maneira harmoniosa. ")
 
 class Ap_Duplo(Quartos):
     def listar_ap_duplo(self):
-        print("Este moderno apartamento duplo é uma solução elegante e prática para acomodar duas pessoas com estilo e conforto. Localizado em um bairro vibrante, oferece um espaço versátil onde cada morador pode desfrutar de sua própria privacidade, ao mesmo tempo em que compartilham uma atmosfera de convivência harmoniosa.")
-        os.system("pause")
-
+        print("Este moderno apartamento duplo é uma solução elegante e prática para acomodar duas pessoas com estilo e conforto. Localizado em um bairro vibrante, oferece um espaço versátil onde cada morador pode desfrutar de sua própria privacidade, ao mesmo tempo em que reúne uma atmosfera de convivência harmoniosa.")
+        
 class Ap_Duplo_casal(Quartos):
     def listar_ap_duplo_casal(self):
-        print("Este encantador apartamento duplo para casal é um espaço versátil e convidativo, projetado para oferecer acomodação confortável e flexível para dois casais. Situado em um bairro tranquilo e de fácil acesso, este apartamento oferece uma combinação de privacidade e convivência.")
-        os.system("pause")
+       print("Este encantador apartamento duplo para casal é um espaço versátil e convidativo, projetado para oferecer acomodação confortável e flexível para dois casais. Situado num bairro tranquilo e de fácil acesso, este apartamento oferece uma combinação de privacidade e convivência.")
