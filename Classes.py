ed0032bc979fb3657f90cc54a3fc9b3527da5f4f
diegorkkj <diegosenaisp@gmail.com>
2023-08-24 07:57:14 -0300

    upload classes

---
+++ Classes.py
@@ -0,0 +1,29 @@
+class Hotel:
+    
+class Quartos(Hotel):
+    def add_quarto():
+        print("")
+
+class Ap_luxo(Quartos):
+    def listar_ap_luxo():
+        print("")
+
+class Ap_Master(Quartos):
+    def listar_ap_master():
+        print("")
+
+class Ap_Simples(Quartos):
+    def listar_ap_simples():
+        print("")
+
+class Ap_Simples_casal(Quartos):
+    def listar_ap_simples_casal():
+        print("")
+
+class Ap_Duplo(Quartos):
+    def listar_ap_duplo():
+        print("")
+
+class Ap_Duplo_casal(Quartos):
+    def listar_ap_duplo_casal():
+        print("")
